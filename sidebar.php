b0a4ef6aed01e544a355946bc6442fc393dd2350
Gustav Pursche <gustavpursche@gmail.com>
2015-07-26 14:19:32 +0200

    Basic aside styling

--- sidebar.php
+++ sidebar.php
@@ -1,1 +1,7 @@
-<h3><?php _e("Recent Reports") ?></h3>
+<img class="aside_image"
+     src="<?php bloginfo('template_url'); ?>/assets/image/wtm-logo.jpg"
+     alt="<?php _e('Watch the med Logo') ?>" />
+
+<h3 class="aside_headline">
+  <?php _e("Recent Reports") ?>
+</h3>
