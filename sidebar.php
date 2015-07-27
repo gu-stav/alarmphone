4dca6f42f4835fcb1671213f39e57e089b17a0e8
Gustav Pursche <gustavpursche@gmail.com>
2015-07-27 13:08:03 +0200

    Remove WTM Logo, Better aside alignment

--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,1 @@
-<img class="aside_image"
-     src="<?php bloginfo('template_url'); ?>/assets/image/wtm-logo.jpg"
-     alt="<?php _e('Watch the med Logo') ?>" />
-
 <?php dynamic_sidebar( 'general' ); ?>
