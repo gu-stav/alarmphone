03bf4095fb72a111f85e938fca265207df3f7836
Gustav Pursche <gustavpursche@gmail.com>
2015-07-27 11:40:56 +0200

    Add basic js for navigation

--- js/dev/main.js
+++ js/dev/main.js
@@ -1,7 +1,7 @@
 requirejs.config({
   baseUrl: '/js/',
   paths: {
-    jquery: '../bower_components/jquery/dist/jquery',
+    jquery: '../../bower_components/jquery/dist/jquery',
   },
   shim: {
     'jquery': {
@@ -10,4 +10,53 @@ requirejs.config({
   },
 });
 
+require(['jquery'], function($) {
+  $(function() {
+    var $navigation = $('.header_navigation-container');
+    var timer;
+    var openClass = 'sub-menu--is-open';
 
+    var toggleMenu = function(e) {
+      var $link = $(this);
+      var $menu = $link.next('ul');
+
+      if($menu.length) {
+        e.preventDefault();
+        $menu.toggleClass(openClass);
+      }
+    };
+
+    var showMenu = function() {
+      var self = this;
+      var $link = $(this);
+      var $menu = $link.next('ul');
+
+      $menu
+        .on('mouseenter', function() {
+          clearTimeout(timer);
+        })
+        .on('mouseleave', function() {
+          hideMenu.apply(self, arguments);
+        })
+        .addClass(openClass);
+    };
+
+    var hideMenu = function() {
+      var $link = $(this);
+      var $menu = $link.next('ul');
+
+      $menu.removeClass('sub-menu--is-open');
+    }
+
+    $navigation
+      .on('focus.navigation', 'a', toggleMenu)
+      .on('mouseenter.navigation', 'a', showMenu)
+      .on('mouseleave.navigation', 'a', function(e) {
+        var self = this;
+
+        timer = setTimeout(function() {
+          hideMenu.apply(self, arguments);
+        }, 200);
+      });
+  });
+});
