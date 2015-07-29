5f4401f1901e10a1accb73a7b36e178c0cac62e7
Gustav Pursche <gustavpursche@gmail.com>
2015-07-29 18:34:21 +0200

    Enhance styling of navigation on small screens, fix archive page structure

--- archive.php
+++ archive.php
@@ -2,47 +2,51 @@
 
 <div class="grid">
   <div class="grid_row">
-    <div class="grid_column grid_column--9">
-      <div class="grid_column grid_column--12">
-        <?php
-          $category = get_the_category();
+    <div class="grid_column grid_column--9 app_content">
+      <div class="grid_row">
+        <div class="grid_column grid_column--12">
+          <?php
+            $category = get_the_category();
 
-          if($category) {
-            $category = $category[0];
-            $category_id = $category->cat_ID;
-            $category_name = $category->name;
-
-            $posts = query_posts(array(
-              'category_id' => $category_id,
-              'post_type' => 'post',
-              'posts_per_page' => -1,
-              'post_status' => 'publish',
-            ));
-          } else {
-            if(is_post_type_archive()) {
-              $qu = get_queried_object();
-              $post_type = $qu->name;
-              $category_name = $qu->labels->name;
+            if($category) {
+              $category = $category[0];
+              $category_id = $category->cat_ID;
+              $category_name = $category->name;
 
               $posts = query_posts(array(
-                'post_type' => $post_type,
+                'category_id' => $category_id,
+                'post_type' => 'post',
                 'posts_per_page' => -1,
                 'post_status' => 'publish',
               ));
-            }
-          }
-        ?>
+            } else {
+              if(is_post_type_archive()) {
+                $qu = get_queried_object();
+                $post_type = $qu->name;
+                $category_name = $qu->labels->name;
 
-        <h1 class="headline headline--h1"><?php _e($category_name) ?></h1>
-
-        <div class="release">
-          <?php
-            if($posts) {
-              foreach($posts as $post) {
-                echo render_blog_post($post, '12', 'release_item', 'preview', null);
+                $posts = query_posts(array(
+                  'post_type' => $post_type,
+                  'posts_per_page' => -1,
+                  'post_status' => 'publish',
+                ));
               }
             }
           ?>
+
+          <h1 class="headline headline--h1"><?php _e($category_name) ?></h1>
+
+          <div class="release">
+            <?php
+              if($posts) {
+                foreach($posts as $post) {
+                  echo '<div class="grid_row">';
+                  echo render_blog_post($post, '12', 'release_item', 'preview', null);
+                  echo '</div>';
+                }
+              }
+            ?>
+          </div>
         </div>
       </div>
     </div>
