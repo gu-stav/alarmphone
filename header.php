0e477303ff57ecf12c588f7222e88ddf20fd5c00
Gustav Pursche <gustavpursche@gmail.com>
2015-07-31 16:42:01 +0200

    Add RSS Feed Tag

--- header.php
+++ header.php
@@ -31,6 +31,10 @@
           content="initial-scale=1" />
     <link rel="shortcut icon"
           href="<?php bloginfo('template_url'); ?>/favicon.ico" />
+    <link rel="alternate"
+          type="application/rss+xml"
+          title="<?php echo $page_name; ?>"
+          href="<?php bloginfo('rss2_url'); ?>" />
 
     <?php
       if( $page_description ) {
