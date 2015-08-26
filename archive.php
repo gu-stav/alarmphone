cc11b2541a7858f352074ab42e787825c71660b7
Gustav Pursche <gustavpursche@gmail.com>
2015-08-26 22:54:02 +0200

    Refactor archive.php logic. Fixes #9

--- archive.php
+++ archive.php
@@ -6,32 +6,32 @@
       <div class="grid_row">
         <div class="grid_column grid_column--12">
           <?php
-            $category = get_the_category();
+            $qu = get_queried_object();
+            $options = array();
+            $args = array(
+              'post_type' => $qu->name,
+              'posts_per_page' => -1,
+              'post_status' => 'publish',
+              'lang' => pll_current_language(),
+            );
 
-            if($category) {
-              $category = $category[0];
-              $category_id = $category->cat_ID;
-              $category_name = $category->name;
+            switch($qu->taxonomy) {
+              case 'category':
+                $category_name = $qu->cat_name;
+                $options = array(
+                  'category_id' => $qu->cat_ID,
+                  'post_type' => 'post',
+                );
+                break;
 
-              $posts = query_posts(array(
-                'category_id' => $category_id,
-                'post_type' => 'post',
-                'posts_per_page' => -1,
-                'post_status' => 'publish',
-              ));
-            } else {
-              if(is_post_type_archive()) {
-                $qu = get_queried_object();
-                $post_type = $qu->name;
-                $category_name = $qu->labels->name;
-
-                $posts = query_posts(array(
-                  'post_type' => $post_type,
-                  'posts_per_page' => -1,
-                  'post_status' => 'publish',
-                ));
-              }
+              default:
+                if(is_post_type_archive()) {
+                  $category_name = $qu->labels->name;
+                }
+                break;
             }
+
+            $posts = query_posts(array_merge($args, $options));
           ?>
 
           <h1 class="headline headline--h1"><?php echo $category_name; ?></h1>
